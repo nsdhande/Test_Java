c3cd32cbfba77d285511ba6ec88723d436bcf68f
nsdhande <144787233+nsdhande@users.noreply.github.com>
2023-09-27 16:03:23 +0530

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,8 +1,5 @@
-class Test2{  
-    public static void main(String args[]){  
-     System.out.println("This is test-2 main");  
-    }  
-    public void testCode(){
-		System.out.println("This is test-2 code");
+class Test2{
+	public void testCode(){
+		System.out.println("This is test-2 testCode() function.");
 	}
-}  
+}
