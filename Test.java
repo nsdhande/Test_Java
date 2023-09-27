0e0c43ab78a8d946f943e39eee9ed0cebded1807
nsdhande <144787233+nsdhande@users.noreply.github.com>
2023-09-27 15:33:49 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,4 +2,6 @@ class Test{
     public static void main(String args[]){  
      System.out.println("\n Welcome to Java world. This is simple program for testing");  
     }  
+    Test2 ts2 = new Test2;
+    Test2();
 }  
