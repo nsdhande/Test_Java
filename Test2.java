e379e629923295ad35217d664c9d556724bddb57
nsdhande <144787233+nsdhande@users.noreply.github.com>
2023-09-27 16:20:13 +0530

    Update Test2.java
    Updated code using second systemOP line

--- Test2.java
+++ Test2.java
@@ -1,11 +1,14 @@
 class Test2{
 	public void testCode1(){
-		System.out.println("This is testCode1() function in Test2.java.");
+		System.out.println("This is testCode1()");
+		System.out.println("\n Welcome to India!");
 	}
 	public void testCode2(){
-		System.out.println("\n This is testCode2 function in Test2.java");
+		System.out.println("\n This is testCode2() ");
+		System.out.println("\n Welcome to Maharashtra!");
 	}
 	public void testCode3(){
-		System.out.println("\n This is testCode3 function in Test2.java");
+		System.out.println("\n This is testCode3() ");
+		System.out.println("\n Welcome to Mumbai!");
 	}
 }
